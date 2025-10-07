233cbe5e840a593ff82e2091aeddc598ca1b33fd
alexaa04 <alevasquez044@gmail.com>
2025-10-07 10:28:45 -0600

    feat(auth): Implementada autenticacion con PHP, MySQL y sesiones. Creado dashboard base.

---
+++ logout.php
@@ -0,0 +1,9 @@
+<?php
+// logout.php
+
+session_start();
+session_unset();   // Limpia todas las variables de sesión
+session_destroy(); // Destruye la sesión actual
+header("Location: login.php");
+exit;
+?>
